1d358198121e8419c223b7e7eadee9f7b17540b0
Mostafa <mostafaamr99533@gmail.com>
2025-11-04 17:52:29 +0200

    + update backgroundColor in appbar.

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.amberAccent,
+        backgroundColor: Colors.green,
       ),
     );
   }
