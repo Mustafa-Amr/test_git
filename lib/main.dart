7acca0c18c3583114be764b386089a9562739e92
Mostafa <mostafaamr99533@gmail.com>
2025-11-04 17:44:46 +0200

    + added backgroundColor in appbar.

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amberAccent,
+      ),
+    );
   }
 }
